f34939bb8a990ad9a74553c58b4a4a04ccb29b2a
Eduardo SO <eduardo.sool07@gmail.com>
2020-01-06 14:26:46 -0300

    Sending email on appointment cancellation

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: 'smtp.mailtrap.io',
+    port: 2525,
+    secure: false,
+    auth: {
+        user: 'f3cdb3445cadab',
+        pass: 'ed8b179de1a341',
+    },
+    default: {
+        from: 'Equipe Gympoint <noreply@gympoint.com.br>',
+    },
+};
