d05d12db9df58e60e2e7aa64ccaf4ee4a0e7a602
Eduardo SO <eduardo.sool07@gmail.com>
2020-01-07 11:19:48 -0300

    Add appointment hour availability

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import multerConfig from './config/multer';
 import UserController from './app/controllers/UserController';
 import StudentController from './app/controllers/StudentController';
 import ProviderController from './app/controllers/ProviderController';
+import AvailabilityController from './app/controllers/AvailabilityController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
@@ -23,6 +24,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailabilityController.index);
 
 routes.put('/users', UserController.update);
 
